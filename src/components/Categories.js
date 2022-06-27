ad901a8bac37b578c713cd5b83d40decba769d36
Aaron <aaron@Aarons-Air.lan>
2022-06-26 20:25:57 -0400

    added click functionality for arrow and background

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,12 +2,21 @@ import React from "react";
 
 export default class Categories extends React.Component {
     render() {
+
+        const isClicked = (e) => {
+            e.preventDefault()
+            document.getElementById('categories').classList.toggle('clicked')
+            document.getElementById('cad').classList.toggle('arrow-visible')
+            this.props.handleClick()
+        }
+
         return (
-            <div className="nav-categories-container nav-item">
-                <a href="/" className="nav-categories">Categories</a>
-                <div className="arrow-holder">
-                    <i className="arrow-down"></i>
-                </div>
+            <div id="categories" className="nav-categories-container nav-item" onClick={isClicked}>
+                <a href="/" className="nav-categories">Categories
+                    <div className="arrow-holder">
+                        <i id="cad" className="arrow"></i>
+                    </div>
+                </a>
             </div>
         )
     }
