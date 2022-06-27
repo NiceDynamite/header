67a5e0a57ad5031201dfed9387730e9c83179eb2
Aaron <aaron@Aarons-Air.lan>
2022-06-26 20:24:42 -0400

    added modals and state for those modals

--- src/app.js
+++ src/app.js
@@ -1,17 +1,43 @@
 import React from "react";
+import CategoriesModal from "./components/CategoriesModal";
+import DealsModal from "./components/DealsModal";
 import Navbar from "./components/Navbar";
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            super: 'fun'
+            CategoriesModal: false,
+            DealsModal: false
         }
     }
 
     render() {
+
+        const categoriesClicked = () => {
+            this.setState({ CategoriesModal: !this.state.CategoriesModal, DealsModal: false })
+        }
+
+        const dealsClicked = () => {
+            this.setState({ DealsModal: !this.state.DealsModal, CategoriesModal: false })
+        }
+
+        const props = {
+            categoriesClicked,
+            dealsClicked
+        }
+
+        const show = {
+            categories: this.state.CategoriesModal,
+            deals: this.state.DealsModal
+        }
+
         return (
-            <Navbar />
+            <>
+                <Navbar props={props} show={show} />
+                <CategoriesModal show={this.state.CategoriesModal} />
+                <DealsModal show={this.state.DealsModal} />
+            </>
         )
     }
 }
