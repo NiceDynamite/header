6b2e0ee7609536662814eca4c270de373185018a
Aaron <aaron@Aarons-Air.lan>
2022-06-26 20:26:55 -0400

    added click functionality for arrow and background

--- src/components/Deals.js
+++ src/components/Deals.js
@@ -2,12 +2,19 @@ import React from "react";
 
 export default class Deals extends React.Component {
     render() {
+        const isClicked = (e) => {
+            e.preventDefault()
+            document.getElementById('deals').classList.toggle('clicked')
+            document.getElementById('dad').classList.toggle('arrow-visible')
+            this.props.handleClick()
+        }
         return (
-            <div className="nav-deals-container nav-item">
-                <a href="/" className="nav-deals">Deals</a>
-                <div className="arrow-holder">
-                    <i className="arrow-down"></i>
-                </div>
+            <div id="deals" className="nav-deals-container nav-item" onClick={isClicked}>
+                <a href="/" className="nav-deals">Deals
+                    <div className="arrow-holder">
+                        <i id="dad" className="arrow"></i>
+                    </div>
+                </a>
             </div>
         )
     }
