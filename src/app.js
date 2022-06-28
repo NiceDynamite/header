6436aaa08c908799f5eb04524ef1bd5f749a1527
Aaron <aaron@Aarons-Air.lan>
2022-06-28 14:52:33 -0400

    added modal functions to what's new and pickup

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from "react";
 import Footer from "./components/Footer";
 import CategoriesModal from "./components/modals/CategoriesModal";
 import DealsModal from "./components/modals/DealsModal";
+import PickupModal from "./components/modals/PickupModal";
+import WhatsNewModal from "./components/modals/WhatsNewModal";
 import Navbar from "./components/Navbar";
 
 class App extends React.Component {
@@ -9,35 +11,57 @@ class App extends React.Component {
         super(props)
         this.state = {
             CategoriesModal: false,
-            DealsModal: false
+            DealsModal: false,
+            WhatsNewModal: false,
+            PickupModal: false,
         }
     }
 
     render() {
 
+        const queryItem = (value) => {
+            fetch(`http://localhost:3001/api/item/${value}`)
+                .then(result => result.json())
+                .then((data) => console.log(data[0].id))
+        }
+
         const categoriesClicked = () => {
-            this.setState({ CategoriesModal: !this.state.CategoriesModal, DealsModal: false })
+            this.setState({ CategoriesModal: !this.state.CategoriesModal, DealsModal: false, WhatsNewModal: false, PickupModal: false })
         }
 
         const dealsClicked = () => {
-            this.setState({ DealsModal: !this.state.DealsModal, CategoriesModal: false })
+            this.setState({ DealsModal: !this.state.DealsModal, CategoriesModal: false, WhatsNewModal: false, PickupModal: false })
+        }
+
+        const whatsNewClicked = () => {
+            this.setState({ WhatsNewModal: !this.state.WhatsNewModal, DealsModal: false, CategoriesModal: false, PickupModal: false })
+        }
+
+        const pickupClicked = () => {
+            this.setState({ PickupModal: !this.state.PickupModal, WhatsNewModal: false, DealsModal: false, CategoriesModal: false })
         }
 
         const props = {
             categoriesClicked,
-            dealsClicked
+            dealsClicked,
+            whatsNewClicked,
+            pickupClicked,
+            queryItem
         }
 
         const show = {
             categories: this.state.CategoriesModal,
-            deals: this.state.DealsModal
+            deals: this.state.DealsModal,
+            whatsNew: this.state.WhatsNewModal
         }
 
         return (
             <>
-                <Navbar props={props} show={show} />
+                <Navbar props={props} />
                 <CategoriesModal show={this.state.CategoriesModal} />
                 <DealsModal show={this.state.DealsModal} />
+                <WhatsNewModal show={this.state.WhatsNewModal} />
+                <PickupModal show={this.state.PickupModal} />
                 <Footer />
             </>
         )
