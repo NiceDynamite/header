88dbe71153b5f8e1d8c67ca93c8e01886a83d7e5
Aaron <aaron@Aarons-Air.lan>
2022-06-29 15:38:33 -0400

    added dotenv and changed port

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const cors = require('cors')
-const pool = require('./database/connection')
+const pool = require('./database/connection.js')
+require('dotenv').config()
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-const PORT = 3001
+const PORT = process.env.PORT || 5001
 
 app.get('/api/item/:name', async (req, res) => {
     try {
         const data = await pool.query('SELECT id FROM target_item WHERE LOWER(item) = LOWER($1);', [req.params.name])
-        res.json(data.rows)
+        res.json(data.rows[0])
     } catch (err) {
         console.error(err.message)
     }
