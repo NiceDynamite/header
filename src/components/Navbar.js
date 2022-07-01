cda8551552b3159b2aceb6b39219d7402fc43a59
Aaron <aaron@Aarons-Air.lan>
2022-06-30 20:30:15 -0400

    added static top nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,16 @@ import WhatsNew from "./WhatsNew";
 import Pickup from "./Pickup";
 import SearchBar from "./SearchBar";
 import SignIn from "./SignIn";
+import TopNav from "./TopNav"
 import TargetLogo from "./TargetLogo";
 
 export default class Navbar extends React.Component {
     render() {
         return (
             <div className="header-wrapper">
+                <div className="static-navbar">
+                    <TopNav />
+                </div>
                 <div className="primary-navbar-container container-fixed">
                     <div className="navbar-container">
                         <nav id="nav" className="primary-nav">
